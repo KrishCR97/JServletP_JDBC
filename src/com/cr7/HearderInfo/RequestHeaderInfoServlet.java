e96e384a6083388d1e655799f5521660129a2fb9
Veera Kishon Kumar Mucherla <veerakishon.mucherla@gmail.com>
2018-06-12 14:11:50 -0400

    Modified web.xml and created html files

--- src/com/cr7/HearderInfo/RequestHeaderInfoServlet.java
+++ src/com/cr7/HearderInfo/RequestHeaderInfoServlet.java
@@ -1,6 +1,9 @@
 package com.cr7.HearderInfo;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,29 +14,43 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class RequestHeaderInfoServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RequestHeaderInfoServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	public RequestHeaderInfoServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.print("<p>" + request.getProtocol() + "</p>");
+		out.println(request.getParameter("userName"));
+		out.println(request.getParameter("password"));
+		Enumeration<String> paramValues = request.getParameterNames();
+		while(paramValues.hasMoreElements()) {
+			out.print(paramValues.nextElement());
+		}
+
 	}
 
 }
